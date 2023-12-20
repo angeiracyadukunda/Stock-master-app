7c786f8b4c7a200e78c46dce64bdcc746038df4c
dveloper-Elisa <kwizeraelisa77@gmail.com>
2023-12-20 23:23:27 +0200

    fetching data from back end

--- js/purchase.js
+++ js/purchase.js
@@ -1,20 +1,49 @@
-//  purchasing validation 
- 
-document.getElementById("record-product").addEventListener("click",(e) => {
+//  purchasing validation
+
+document
+  .getElementById("record-product")
+  .addEventListener("click", async (e) => {
     e.preventDefault();
-   
-  const purchasedProd = document.getElementById('purchasedProd').value;
-  
- const quantityNumber = document.getElementById('quantityNumber').value;
 
- const pricePerUnit = document.getElementById('pricePurchase').value;
+    const purchasedProd = document.getElementById("purchasedProd").value;
 
- if(purchasedProd === "" || quantityNumber === "" || pricePerUnit === ""){
-    alert("please fill out all fields");
-    
- }else{
-    alert("product purchased successfully")
- }
+    const quantityNumber = document.getElementById("quantityNumber").value;
 
+    const pricePerUnit = document.getElementById("pricePurchase").value;
 
-  })
+    if (purchasedProd === "" || quantityNumber === "" || pricePerUnit === "") {
+      alert("Fill out all fields");
+    } else {
+      // STARTING FETCHING PROCESS
+
+      try {
+        const name = document.getElementById("purchasedProd").value;
+        const quantity = document.getElementById("quantityNumber").value;
+        const pricePerUnit = document.getElementById("pricePurchase").value;
+        const newProduct = {
+          name,
+          quantity,
+          pricePerUnit,
+        };
+
+        const token = localStorage.getItem("token");
+        const url = "http://localhost:3000/product";
+        await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(newProduct),
+        })
+          .then((product) => product.json())
+          .then((recorded) => {
+            console.log(recorded);
+            alert(recorded.mesage);
+          })
+          .catch((error) => console.log(error));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  });
