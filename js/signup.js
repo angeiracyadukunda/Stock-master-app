61f32185519f7952b1aebe22100d3483586280a3
dveloper-Elisa <kwizeraelisa77@gmail.com>
2023-12-21 22:58:33 +0200

    amend

--- js/signup.js
+++ js/signup.js
@@ -1,27 +1,50 @@
-document.getElementById ('signup').addEventListener('click', (e) => {
-    e.preventDefault();
+document.getElementById("signup").addEventListener("click", (e) => {
+  e.preventDefault();
 
-    const yourName = document.getElementById('yourName').value;
-    const yourEmail = document.getElementById('yourEmail').value;
-    const yourPass = document.getElementById('yourPass').value;
-    const comfirmPass = document.getElementById('comfirmPass').value;
-    const  checkbox = document.getElementById('checkbox').value;
+  const yourName = document.getElementById("yourName").value;
+  const yourEmail = document.getElementById("yourEmail").value;
+  const yourPass = document.getElementById("yourPass").value;
+  const comfirmPass = document.getElementById("comfirmPass").value;
 
-    let strongPassword = "";
+  let strongPassword = "";
 
-    if(yourPass.length >= 8 ){
-        strongPassword = yourPass;
-      
-    }else{
-      return  alert ('password must be at least 8 characters');
+  if (yourPass.length >= 8) {
+    strongPassword = yourPass;
+  } else {
+    return alert("password must be at least 8 characters");
+  }
+  if (
+    yourName === "" ||
+    yourEmail === "" ||
+    yourPass === "" ||
+    comfirmPass === ""
+  ) {
+    return alert("fill out all fields");
+  }
+  if (strongPassword === comfirmPass) {
+    const createAccount = async () => {
+      try {
+        const userInfo = {
+          yourName,
+          yourEmail,
+          strongPassword,
+        };
 
-    }
-    if(yourName==="" || yourEmail==="" || yourPass==="" || comfirmPass===""){
-         return alert ('fill out all fields');
-    } if(strongPassword === comfirmPass ){
-
-            return alert("information are correct");
-    } 
-
-})
-
+        const url = "http:localhost:3000/users/signup";
+        await fetch(url, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(userInfo),
+        })
+          .then((data) => data.json())
+          .then((response) => console.log(response))
+          .catch((error) => console.log(error));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    createAccount();
+  } else {
+    return alert("Password Should match");
+  }
+});
